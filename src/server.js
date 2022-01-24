392523f4546a569675cb0535a65dbacae617a5a0
Victoria Olivieri <70556187+vickyrose456@users.noreply.github.com>
2022-01-23 20:05:35 -0500

    Completed Simple Server up to #35 on HW.

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const htmlHandler = require('./htmlResponses.js');
-// const textHandler = require('./src/textResponses.js');
-// const jsonHandler = require('./src/jsonResponses.js');
+const textHandler = require('./textResponses.js');
+const jsonHandler = require('./jsonResponses.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
@@ -14,6 +14,18 @@ const onRequest = (request, response) => {
       break;
     case '/page2':
       htmlHandler.getPage2(request, response);
+      break;
+    case '/hello':
+      textHandler.getHello(request, response);
+      break;
+    case '/time':
+      textHandler.getTime(request, response);
+      break;
+    case '/helloJSON':
+      jsonHandler.getHelloJSON(request, response);
+      break;
+    case '/timeJSON':
+      jsonHandler.getTimeJSON(request, response);
       break;
     default:
       htmlHandler.getIndex(request, response);
